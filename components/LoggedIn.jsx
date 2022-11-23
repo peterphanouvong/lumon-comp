7ce6b833073b071f9d32b1ee0f963e5a46ed0757
Peter Phanouvong <peterphanouvong@gmail.com>
2022-11-23 14:37:42 +1100

    init

--- components/LoggedIn.jsx
+++ components/LoggedIn.jsx
@@ -1,62 +1,87 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const LoggedIn = () => {
+  const [profilePic, setProfilePic] = useState("/profile-1.png");
   const { user } = useKindeAuth();
-  console.log(user);
-  return (
-    <>
-      <header>
-        <nav className="nav container">
-          <h1 className="text-display-3">KindeAuth</h1>
-          <div className="profile-blob">
-            <div className="avatar">
-              {user.first_name[0]}
-              {user.first_name[1]}
-            </div>
-            <div>
-              <p className="text-heading-2">
-                {user.first_name} {user.last_name}
-              </p>
-              <Link className="text-subtle" href="/api/auth/logout">
-                Sign out
-              </Link>
-            </div>
-          </div>
-        </nav>
-      </header>
 
-      <main>
-        <div className="container">
-          <div className="card start-hero">
-            <p className="text-body-2 start-hero-intro">Woohoo!</p>
-            <p className="text-display-2">
-              Your authentication is all sorted.
-              <br />
-              Build the important stuff.
-            </p>
+  useEffect(() => {
+    switch (user.first_name) {
+      case "Helena":
+        setProfilePic("/profile-1.png");
+        break;
+      case "Mark":
+        setProfilePic("/profile-3.png");
+        break;
+      case "Dylan":
+        setProfilePic("/profile-4.png");
+        break;
+      case "Irving":
+        setProfilePic("/profile-2.png");
+        break;
+      default:
+        break;
+    }
+  }, [user]);
+  return (
+    <div className="logged-in-layout">
+      <main className="container">
+        <div className="flip-card">
+          <div className="flip-card-inner ">
+            <article className="access-card">
+              <div className="access-card__logo">
+                <Image
+                  src="/logo-green.png"
+                  alt="Lumon logo"
+                  width={260}
+                  height={38}
+                />
+              </div>
+              <dl>
+                <dt>hello, my name is</dt>
+                <dd className="flex-between">
+                  <span>
+                    {user.first_name} {user.last_name}
+                  </span>
+                  <span>SVR'D ACCESS</span>
+                </dd>
+                <dt>employee code</dt>
+                <dd>08-{user.id.substr(-3)}</dd>
+                <dt className="float-right">signature</dt>
+              </dl>
+              <div className="access-card__profile-pic">
+                <Image
+                  src={profilePic}
+                  alt="Profile picture"
+                  width={280}
+                  height={240}
+                />
+              </div>
+            </article>
+
+            <article className="access-card access-card--back">
+              <Image
+                src={"/qr-code.png"}
+                width={200}
+                height={200}
+                alt="qr code"
+              />
+            </article>
           </div>
-          <section className="next-steps-section">
-            <h2 className="text-heading-1">Next steps for you</h2>
-          </section>
         </div>
       </main>
-
-      <footer className="footer">
-        <div className="container">
-          <strong className="text-heading-2">KindeAuth</strong>
-          <p className="footer-tagline text-body-3">
-            Visit our{" "}
-            <Link className="link" href="https://kinde.com/docs">
-              help center
-            </Link>
-          </p>
-
-          <small className="text-subtle">
-            © 2022 KindeAuth, Inc. All rights reserved
-          </small>
-        </div>
+      <footer className="logged-in-footer">
+        <Link href="/api/auth/logout">
+          <button autoFocus className="retro-button">
+            <span className="retro-button__inner">
+              <span className="retro-button__text">Sign out</span>
+            </span>
+          </button>
+        </Link>
       </footer>
-    </>
+    </div>
   );
 };
--- components/LoggedOut.jsx
+++ components/LoggedOut.jsx
@@ -1,57 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export const LoggedOut = () => {
   return (
     <>
-      <header>
-        <nav className="nav container">
-          <h1 className="text-display-3">KindeAuth</h1>
-          <div>
-            <Link className="btn btn-ghost sign-in-btn" href="/api/auth/login">
-              Sign in
-            </Link>
-            <Link className="btn btn-dark" href="/api/auth/register">
-              Sign up
-            </Link>
+      <main className="center">
+        <div className="container pb-5">
+          <div className="spacing-3">
+            <Image src="/logo.png" alt="Lumon logo" width={120} height={52.8} />
           </div>
-        </nav>
-      </header>
-
-      <main>
-        <div className="container">
-          <div className="card hero">
-            <p className="text-display-1 hero-title">
-              Let's start authenticating <br /> with KindeAuth
-            </p>
-            <p className="text-body-1 hero-tagline">Configure your app</p>
-
-            <Link
-              href="https://kinde.com/docs/sdks/nextjs-sdk"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-light btn-big"
-            >
-              Go to docs
+          <p className="text-body-2 spacing-2">
+            Come now, children of my industry, and know the children of my
+            blood.
+          </p>
+          <div>
+            <Link className="btn btn-dark" href="/api/auth/login">
+              SVRD ACCESS
             </Link>
           </div>
         </div>
       </main>
 
-      <footer className="footer">
-        <div className="container">
-          <strong className="text-heading-2">KindeAuth</strong>
-          <p className="footer-tagline text-body-3">
-            Visit our{" "}
-            <Link className="link" href="https://kinde.com/docs">
-              help center
-            </Link>
-          </p>
-
-          <small className="text-subtle">
-            © 2022 KindeAuth, Inc. All rights reserved
-          </small>
-        </div>
-      </footer>
+      <footer className="footer"></footer>
     </>
   );
 };
